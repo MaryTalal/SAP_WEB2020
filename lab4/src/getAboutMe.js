95723452b673b6cea3393c92509df0399831093f
MaryTalal <52213678+MaryTalal@users.noreply.github.com>
2020-05-20 14:06:42 +0300

    1

---
+++ lab4/src/getAboutMe.js
@@ -0,0 +1,4 @@
+export default function addName() {
+    let elem = document.getElementById("app")
+    elem.innerHTML = "Талаленко Мария Владимировна M3306";
+}
---
+++ lab4/src/index.js
@@ -0,0 +1,12 @@
+import getPokemonInfo from './Pockemon.js';
+import getAboutMe from './getAboutMe.js';
+const url = 'https://pokeapi.co/api/v2/pokemon/ditto/';
+function addPokemon() {
+    return getPokemonInfo(url);
+}
+
+onload = function loadPage() {
+    document.getElementById("AboutMeTab").onclick=addName;
+    document.getElementById("PokemonTab").onclick=addPokemon;
+    addName();
+}
