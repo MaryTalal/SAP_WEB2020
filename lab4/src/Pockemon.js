3e3c45bfbce1b1d8a48ee09cf93bfbff97a54256
MaryTalal <52213678+MaryTalal@users.noreply.github.com>
2020-05-20 16:17:35 +0300

    pockemon.js

--- lab4/src/Pockemon.js
+++ lab4/src/Pockemon.js
@@ -1,25 +1,23 @@
 export default async function getPokemonInfo(url) {
     try {
-        let [response] = await Promise.all([fetch(url)]);
+        const response = await fetch(url);
         if (response.ok) {
-            let [json] = await Promise.all([response.json()]);
+            const json = await response.json();
             const name = ("<span>" + "Покемон: " + json.forms[0].name + "<br>" + "</span>"),
-                abilities = ("<span>" + "Способности: " + json.abilities[0].ability.name + "<br>" + "</span>");
+            abilities = ("<span>" + "Способности: " + json.abilities[0].ability.name + "<br>" + "</span>");
             let effect = [];
             for (let i = 0; i < json.abilities.length; i++) {
-                let [response] = await Promise.all([fetch(json.abilities[i].ability.url)]);
-                if (response.ok) {
-                    let json2 = await response.json();
-                    effect += "<span>" + "Cпособность: " + (json2.effect_entries[0].effect) + "<br>" + "</span>";
+                const abilitiesResponse = await fetch(json.abilities[i].ability.url);
+                if (abilitiesResponse.ok) {
+                    let abilitiesJson = await abilitiesResponse.json();
+                    effect.push("<span>" + "Cпособность: " + (abilitiesJson.effect_entries[0].effect) + "<br>" + "</span>");
                 }
             }
             let elem = document.getElementById("app");
             elem.innerHTML = name + abilities + effect;
         }
     } catch (e) {
-        let elem = document.getElementById("app");
-        elem.innerHTML = "ERROR " + e.toString();
-    }
-
+         let elem = document.getElementById("app");
+         elem.innerHTML = "ERROR " + e.toString();
+     }
 }
-
